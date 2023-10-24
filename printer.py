f9760b810500e936f43b180c0e0c85b23d462100
IlyaDenisov88 <113009175+IlyaDenisov88@users.noreply.github.com>
2023-10-24 14:07:38 +0300

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,6 +1,6 @@
-import random
+
 with open ("numbers.txt", "w") as file:
-  for line in range(0, random.randint(1, 100)):
+  for line in range(0, 2):
     file.write(str(line))
     file.write('\n')
   file.close()
